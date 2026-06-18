dfd663aa22ff36b99106b8447e6d202bc633945a
Cruz <cruz.x@icloud.com>
2026-06-19 00:30:06 +0800

    Mount world map and cat album UI

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import * as THREE from "three";
 import { createGameScene } from "./scene/world.js";
 import { setupNativeBridge } from "./native/capacitor-bridge.js";
 import { mountGameShell } from "./ui/game-shell.js";
+import { mountVisionUpgrade } from "./ui/vision-upgrade.js";
 
 const runtime = createGameScene({ THREE });
 setupNativeBridge(runtime);
 mountGameShell(runtime);
+mountVisionUpgrade(runtime);
